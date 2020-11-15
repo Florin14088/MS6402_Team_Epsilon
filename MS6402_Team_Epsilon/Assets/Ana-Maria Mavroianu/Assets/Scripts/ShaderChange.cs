a0de33a921d4cf1575a1a5ba9518ec4c22acedd8
ana-mavroianu <58072207+ana-mavroianu@users.noreply.github.com>
2020-11-15 13:59:37 +0000

    Added 2 shaders and a character
    One shader for NPC with a dissolving effect. It has an attached script that let's you change between shaders.
    One shader for TV static.
    A character that looks like a doctor.

---
+++ MS6402_Team_Epsilon/Assets/Ana-Maria Mavroianu/Assets/Scripts/ShaderChange.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShaderChange : MonoBehaviour
+{
+    public List<Material> matList = new List<Material>();
+
+    private Shader shader_dissolv;
+    private Shader shader_lit;
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        shader_dissolv = Shader.Find("Shader Graphs/Dissolve");    // asign shaders value
+        shader_lit = Shader.Find("Lightweight Render Pipeline/Lit");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            foreach(Material m in matList)
+            {
+                if (m.shader == shader_lit) m.shader = shader_dissolv;
+                else m.shader = shader_lit;
+            }
+        }
+    }
+
+
+
+}
