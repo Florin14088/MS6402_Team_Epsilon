28a5b277a9dba598359c49a4054657f2a83b2b47
Florin14088 <58071952+Florin14088@users.noreply.github.com>
2020-11-21 11:32:44 +0000

    Added Functionalities to Main Menu UI
    Added buttons that work, windows, scrolling texts, etc. What you would expect to find in an UI is there

---
+++ MS6402_Team_Epsilon/Assets/Florin-Bogdan Glod/02__F_Scripts/Auto_Move_UI_Anchors.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[ExecuteInEditMode]
+public class Auto_Move_UI_Anchors : MonoBehaviour
+{
+    private RectTransform t;
+    private RectTransform pt;
+
+    void Start()
+    {
+        t = gameObject.GetComponent<RectTransform>();
+        pt = gameObject.GetComponent<RectTransform>().parent as RectTransform;
+
+        if (t == null || pt == null) return;
+
+        Vector2 newAnchorsMin = new Vector2(t.anchorMin.x + t.offsetMin.x / pt.rect.width,
+                                         t.anchorMin.y + t.offsetMin.y / pt.rect.height);
+        Vector2 newAnchorsMax = new Vector2(t.anchorMax.x + t.offsetMax.x / pt.rect.width,
+                                         t.anchorMax.y + t.offsetMax.y / pt.rect.height);
+
+        t.anchorMin = newAnchorsMin;
+        t.anchorMax = newAnchorsMax;
+        t.offsetMin = t.offsetMax = new Vector2(0, 0);
+
+        Auto_Move_UI_Anchors _script_AUTOanchor = gameObject.GetComponent<Auto_Move_UI_Anchors>();
+        DestroyImmediate(_script_AUTOanchor);//sterge componentul automat
+    }
+}
