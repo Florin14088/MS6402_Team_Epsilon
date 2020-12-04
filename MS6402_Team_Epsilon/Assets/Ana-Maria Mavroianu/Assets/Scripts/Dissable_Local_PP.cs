8e2c1449db4128ff4ccc15e3a7f939495e54d10c
ana-mavroianu <58072207+ana-mavroianu@users.noreply.github.com>
2020-12-04 17:35:31 +0000

    Added Local PP Objects and sounds
    For the fear effects.

---
+++ MS6402_Team_Epsilon/Assets/Ana-Maria Mavroianu/Assets/Scripts/Dissable_Local_PP.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dissable_Local_PP : MonoBehaviour
+{
+    public GameObject local_PP;
+    public GameObject local_PP_02;
+    [Space]
+    public bool enable_1;
+    public bool enable_2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        local_PP.SetActive(false);
+        local_PP_02.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (enable_1 && local_PP.activeSelf == false)
+        {
+            enable_2 = false;
+            local_PP.SetActive(true);
+            local_PP_02.SetActive(false);
+        }
+
+        if (enable_2 && local_PP_02.activeSelf == false)
+        {
+            enable_1 = false;
+            local_PP.SetActive(false);
+            local_PP_02.SetActive(true);
+        }
+    }
+}
